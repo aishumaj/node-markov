d005e1c47d549c65a3e596d17828a16999587520
Aishwarya Majumdar <aishu.maj1996@gmail.com>
2022-08-16 16:40:39 -0700

    implementation

--- markov.js
+++ markov.js
@@ -1,3 +1,4 @@
+"use strict";
 /** Textual markov chain generator. */
 
 
@@ -15,7 +16,7 @@ class MarkovMachine {
   /** Get markov chain: returns Map of Markov chains.
    *
    *  For text of "The cat in the hat.", chains will be:
-   * 
+   *
    *  {
    *   "The": ["cat"],
    *   "cat": ["in"],
@@ -23,11 +24,28 @@ class MarkovMachine {
    *   "the": ["hat."],
    *   "hat.": [null],
    *  }
-   * 
+   *
    * */
 
   getChains() {
-    // TODO: implement this!
+    //loop thru this.words
+    //put them into an object where key is word and value is next word
+    let markovChains = new Map();
+    const words = this.words;
+
+    for (let i = 0; i < words.length; i++) {
+      if (i < words.length - 1) {
+        markovChains.has(words[i]) ?
+          markovChains.get(words[i]).push(words[i+1]) :
+          markovChains.set(words[i], [words[i+1]]);
+      } else {
+        markovChains.has(words[i]) ?
+          markovChains.get(words[i]).push(null) :
+          markovChains.set(words[i], [null]);
+      }
+    }
+
+    return markovChains;
   }
 
 
@@ -35,10 +53,37 @@ class MarkovMachine {
    *  until it hits a null choice. */
 
   getText() {
-    // TODO: implement this!
-
     // - start at the first word in the input text
     // - find a random word from the following-words of that
     // - repeat until reaching the terminal null
+
+    //create empty array, push each word into array and then convert to string
+    const words = this.words;
+    let text = [words[0]];
+
+    //push a random word from value array
+    let nextWord;
+
+    do {
+      console.log(this.chains.get(text[text.length - 1]));
+      nextWord = getRandomItem(this.chains.get(text[text.length - 1]));
+      console.log(nextWord);
+      text.push(nextWord);
+    }
+    while (nextWord !== null);
+
+    return text.join(" ");
   }
+}
+
+/** Given an input array, picks a random element from the array and returns. */
+function getRandomItem(arr) {
+
+  // get random index value
+  const randomIndex = Math.floor(Math.random() * arr.length);
+
+  // get random item
+  const item = arr[randomIndex];
+
+  return item;
 }
